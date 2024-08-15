823e571d4f4bc197918bbb2d97e45676fd0f186b
Teguh Ainul Darajat <21670357+teguhdarajat@users.noreply.github.com>
2024-08-15 13:48:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ from mini_program import MiniProgram
 if __name__ == "__main__":
     mini_program = MiniProgram(100)
     numbers = mini_program.get_numbers()
-    print(numbers)
     for i in range(len(numbers)):
         print(numbers[i], end = " ")
